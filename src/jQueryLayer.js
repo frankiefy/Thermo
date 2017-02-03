b263696ddca0d59644e332503534bea6d4791ad4
frankie <franklin.shaw@gmail.com>
2017-02-03 14:52:55 +0000

    weather information

--- src/jQueryLayer.js
+++ src/jQueryLayer.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=6c6c983ae4efa3d5b78c355cdb07ea77&units=metric', function(data){
+    $('#current-temperature').text(data.main.temp);
+    $('#current-humidity').text(data.main.humidity)
+    $('#current-pressure').text(data.main.pressure)
+    $('#current-min-temperature').text(data.main.temp_min)
+    $('#current-max-temperature').text(data.main.temp_max)
+    $('#location-name').text(data.name)
+})
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=6c6c983ae4efa3d5b78c355cdb07ea77&units=metric', function(data){
+      $('#current-temperature').text(data.main.temp)
+      $('#current-humidity').text(data.main.humidity)
+      $('#current-pressure').text(data.main.pressure)
+      $('#current-min-temperature').text(data.main.temp_min)
+      $('#current-max-temperature').text(data.main.temp_max)
+      $('#location-name').text(data.name)
+    })
+  })
+
   var thermostat = new Thermostat();
   updateDisplay();
 
